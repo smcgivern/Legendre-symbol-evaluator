230a3dc04dd6663b0e61d4d27b8c49865ce768e6
Sean McGivern <sean@mcgivern.me.uk>
2009-12-26 13:37:56 +0000

    Untabified main JS file

--- ext/legendre.js
+++ ext/legendre.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('#form').submit(evaluateLegendre);
+    $('#form').submit(evaluateLegendre);
 });
 
 var pow = Math.pow;
@@ -12,140 +12,140 @@ function isComposite(n) { return !(isOddPrime(n) || ((0 <= n) && (n <= 2))); }
 function isOddPrime(n) { return (isPrime(n) && n != 2); }
 
 function isPrime(n) {
-	if ((n < 3) || (mod(n, 2) == 0)) { return (n == 2); }
+    if ((n < 3) || (mod(n, 2) == 0)) { return (n == 2); }
 
-	for (var i = 3; n / i >= i; i += 2) {
-		if (mod(n, i) == 0) { return false; }
-	}
+    for (var i = 3; n / i >= i; i += 2) {
+        if (mod(n, i) == 0) { return false; }
+    }
 
-	return true;
+    return true;
 }
 
 function primeFactors(n) {
-	var fs = new Array();
+    var fs = new Array();
 
-	if (isPrime(n)) { return [n]; }
+    if (isPrime(n)) { return [n]; }
 
-	for (var i = 2; n / i >= 1; i++) {
-		if (isInt(n / i) && isPrime(i)) {
-			for (var j = 1; n >= pow(i, j); j++) {
-				if (mod(n, pow(i, j)) == 0) { fs.push(i); }
-			}
-		}
-	}
+    for (var i = 2; n / i >= 1; i++) {
+        if (isInt(n / i) && isPrime(i)) {
+            for (var j = 1; n >= pow(i, j); j++) {
+                if (mod(n, pow(i, j)) == 0) { fs.push(i); }
+            }
+        }
+    }
 
-	return fs;
+    return fs;
 }
 
 var steps = [
-	{
-		'id': 'p-odd-prime',
-		'check': function (x, y) { return !isOddPrime(y); },
-		'result': function (x, y) { return [0, 0]; }
-	},
-	{
-		'id': 'a-not-congruent-p',
-		'check': function (x, y) { return (mod(x, y) == 0) },
-		'result': function (x, y) { return [0, 0]; }
-	},
-	{
-		'id': 'quadratic-character--1',
-		'check': function (x, y) { return ((x == -1) || (x == y - 1)); },
-		'result': function (x, y) { return [pow(-1, (y - 1) / 2), 0]; }
-	},
-	{
-		'id': 'square-numbers',
-		'check': function (x, y) { return isSquare(x); },
-		'result': function (x, y) { return [1, 0]; }
-	},
-	{
-		'id': 'congruent-numbers',
-		'check': function (x, y) { return ((x > y) || (x < -1)); },
-		'result': function (x, y) { return [mod(x, y), y]; }
-	},
-	{
-		'id': 'composite-numbers',
-		'check': function (x, y) { return isComposite(x); },
-		'result': function (x, y) { return [primeFactors(x), y]; }
-	},
-	{
-		'id': 'quadratic-character-2',
-		'check': function (x, y) { return (x == 2); },
-		'result': function (x, y) {	return [pow(-1, (pow(y, 2) - 1) / 8), y]; }
-	},
-	{
-		'id': 'law-quadratic-reciprocity-1',
-		'check': function (x, y) {
-			return (isPrime(x) && isPrime(y) &&
-					(mod(x, 4) == 1 || mod(y, 4) == 1)
-				   );
-		},
-		'result': function (x, y) {	return [y, x]; }
-	},
-	{
-		'id': 'law-quadratic-reciprocity-2',
-		'check': function (x, y) {
-			return (isPrime(x) && isPrime(y) &&
-					(mod(x, 4) == 3 && mod(y, 4) == 3)
-				   );
-		},
-		'result': function (x, y) {	return [[-1, y], x]; }
-	}
+    {
+        'id': 'p-odd-prime',
+        'check': function (x, y) { return !isOddPrime(y); },
+        'result': function (x, y) { return [0, 0]; }
+    },
+    {
+        'id': 'a-not-congruent-p',
+        'check': function (x, y) { return (mod(x, y) == 0) },
+        'result': function (x, y) { return [0, 0]; }
+    },
+    {
+        'id': 'quadratic-character--1',
+        'check': function (x, y) { return ((x == -1) || (x == y - 1)); },
+        'result': function (x, y) { return [pow(-1, (y - 1) / 2), 0]; }
+    },
+    {
+        'id': 'square-numbers',
+        'check': function (x, y) { return isSquare(x); },
+        'result': function (x, y) { return [1, 0]; }
+    },
+    {
+        'id': 'congruent-numbers',
+        'check': function (x, y) { return ((x > y) || (x < -1)); },
+        'result': function (x, y) { return [mod(x, y), y]; }
+    },
+    {
+        'id': 'composite-numbers',
+        'check': function (x, y) { return isComposite(x); },
+        'result': function (x, y) { return [primeFactors(x), y]; }
+    },
+    {
+        'id': 'quadratic-character-2',
+        'check': function (x, y) { return (x == 2); },
+        'result': function (x, y) { return [pow(-1, (pow(y, 2) - 1) / 8), y]; }
+    },
+    {
+        'id': 'law-quadratic-reciprocity-1',
+        'check': function (x, y) {
+            return (isPrime(x) && isPrime(y) &&
+                    (mod(x, 4) == 1 || mod(y, 4) == 1)
+                   );
+        },
+        'result': function (x, y) { return [y, x]; }
+    },
+    {
+        'id': 'law-quadratic-reciprocity-2',
+        'check': function (x, y) {
+            return (isPrime(x) && isPrime(y) &&
+                    (mod(x, 4) == 3 && mod(y, 4) == 3)
+                   );
+        },
+        'result': function (x, y) { return [[-1, y], x]; }
+    }
 ];
 
 function legendreStep(x, y) {
-	for (var i = 0; i < steps.length; i++) {
-		var step = steps[i];
+    for (var i = 0; i < steps.length; i++) {
+        var step = steps[i];
 
-		if (step.check(x, y)) {
-			return {'id': step.id, 'next': step.result(x, y)};
-		}
-	}
+        if (step.check(x, y)) {
+            return {'id': step.id, 'next': step.result(x, y)};
+        }
+    }
 }
 
 function allLegendreSteps(x, y) {
-	function isNotOne(n) { return !(n == 1 || n == -1); }
-	function done(s) { return (allOnes(s[0]) || s[1] == 0); }
+    function isNotOne(n) { return !(n == 1 || n == -1); }
+    function done(s) { return (allOnes(s[0]) || s[1] == 0); }
 
-	function allOnes(a) {
-		return ($.grep($.makeArray(a), isNotOne).length == 0);
-	}
+    function allOnes(a) {
+        return ($.grep($.makeArray(a), isNotOne).length == 0);
+    }
 
-	var step = legendreStep(x, y);
-	var next = step.next;
-	var legendreSteps = [step];
+    var step = legendreStep(x, y);
+    var next = step.next;
+    var legendreSteps = [step];
 
-	while(next[1] != 0) {
-		if ($.isArray(next[0])) {
-			var zs = [];
+    while(next[1] != 0) {
+        if ($.isArray(next[0])) {
+            var zs = [];
 
-			for (var i = 0; i < next[0].length; i++) {
-				substeps = allLegendreSteps(next[0][i], next[1]);
-				legendreSteps.push(substeps);
-				zs.push(last(substeps).next[0]);
-			}
+            for (var i = 0; i < next[0].length; i++) {
+                substeps = allLegendreSteps(next[0][i], next[1]);
+                legendreSteps.push(substeps);
+                zs.push(last(substeps).next[0]);
+            }
 
-			step = {'id': 'composite-numbers', 'next': [zs, 0]};
-			next = step.next;
-			legendreSteps.push(step);
+            step = {'id': 'composite-numbers', 'next': [zs, 0]};
+            next = step.next;
+            legendreSteps.push(step);
 
-		} else {
-			step = legendreStep(step.next[0], step.next[1]);
-			next = step.next;
-			legendreSteps.push(step);
-		}
-	}
+        } else {
+            step = legendreStep(step.next[0], step.next[1]);
+            next = step.next;
+            legendreSteps.push(step);
+        }
+    }
 
-	if ($.isArray(next[0]) && allOnes(next[0])) {
-		var z = 1;
-		for (var i = 0; i < next[0].length; i++) { z = z * next[0][i]; }
+    if ($.isArray(next[0]) && allOnes(next[0])) {
+        var z = 1;
+        for (var i = 0; i < next[0].length; i++) { z = z * next[0][i]; }
 
-		legendreSteps.push(
-			{'id': 'composite-numbers', 'next': [z, 0]}
-		);
-	}
+        legendreSteps.push(
+            {'id': 'composite-numbers', 'next': [z, 0]}
+        );
+    }
 
-	return legendreSteps;
+    return legendreSteps;
 }
 
 function evaluateLegendre() {
